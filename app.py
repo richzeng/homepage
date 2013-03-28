240f1179c8dd22a7d82fe5e89207ef534c1510dc
Richie Zeng <richzeng@gmail.com>
2013-03-28 00:29:05 -0700

    design2

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html')
 
+@app.route("/home2")
+def hello2():
+    return render_template('home2.html')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', port=8000, debug = True)
