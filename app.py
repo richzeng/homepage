73bc933468e95089d10033adea54978f5712d3a2
Richie Zeng <richzeng@gmail.com>
2013-05-30 22:48:38 -0700

    starting to work on better sizing

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug = True)
